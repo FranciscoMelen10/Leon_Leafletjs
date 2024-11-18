51af52ee3cf369e27849fb66859816d6df604de5
Francisco_Melendez <fmelendezsimplina@gmail.com>
2024-11-17 21:51:02 -0600

    Add: La coropleta al mapa y leyenda

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -8,12 +8,34 @@
 // Inicializar el mapa
 var map = L.map("map").setView([12.565287, -86.64917], 9);
 
+function getColor(d) {
+  return d > 50000
+    ? "#1a1a1d"
+    : d > 20000
+    ? "#6f2232"
+    : d > 15000
+    ? "#950740"
+    : d > 10000
+    ? "#c3073f"
+    : d > 5000
+    ? "#ff5733"
+    : d > 1000
+    ? "#ffc300"
+    : d > 0
+    ? "#daf7a6"
+    : "#f7f7f7";
+}
+
 // Estilos del GeoJSON
-const myStyle = {
-  color: "#FF0000",
-  weight: 3,
-  opacity: 1,
-};
+function style(feature) {
+  return {
+    fillColor: getColor(feature.properties.Hombres),
+    weight: 2,
+    opacity: 1,
+    color: "#FF0000",
+    fillOpacity: 0.7,
+  };
+}
 
 // Añadir el mapa base
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -39,32 +61,16 @@ fetch("./layers/LEON.json")
       onEachFeature: function (feature, layer) {
         layer.bindPopup(
           "<table style='border-collapse: collapse; width: 100%;'>" +
-            "<tr><th style='border: 1px solid black; padding: 4px; text-align: left;'>Propiedad</th><th style='border: 1px solid black; padding: 4px; text-align: left;'>Valor</th></tr>" +
-            "<tr><td style='border: 1px solid black; padding: 4px;'>Departamento</td><td style='border: 1px solid black; padding: 4px;'>" +
+            "<tr><td style='border: 1px solid black; padding: 4px; font-weight: 700;'>Departamento</td><td style='border: 1px solid black; padding: 4px;'>" +
             feature.properties.Municipio +
             "</td></tr>" +
-            "<tr><td style='border: 1px solid black; padding: 4px;'>Hombres</td><td style='border: 1px solid black; padding: 4px;'>" +
+            "<tr><td style='border: 1px solid black; padding: 4px; font-weight: 700;'>Hombres</td><td style='border: 1px solid black; padding: 4px;'>" +
             feature.properties.Hombres +
-            "</td></tr>" +
-            "<tr><td style='border: 1px solid black; padding: 4px;'>Mujeres</td><td style='border: 1px solid black; padding: 4px;'>" +
-            feature.properties.Mujeres +
-            "</td></tr>" +
-            "<tr><td style='border: 1px solid black; padding: 4px;'>Total de habitantes</td><td style='border: 1px solid black; padding: 4px;'>" +
-            feature.properties.Total +
-            "</td></tr>" +
-            "<tr><td style='border: 1px solid black; padding: 4px;'>Viviendas</td><td style='border: 1px solid black; padding: 4px;'>" +
-            feature.properties.Viviendas_ +
-            "</td></tr>" +
-            "<tr><td style='border: 1px solid black; padding: 4px;'>Viviendas Urbanas</td><td style='border: 1px solid black; padding: 4px;'>" +
-            feature.properties.VIV_Urbana +
-            "</td></tr>" +
-            "<tr><td style='border: 1px solid black; padding: 4px;'>Viviendas Rurales</td><td style='border: 1px solid black; padding: 4px;'>" +
-            feature.properties.VIV_Rurale +
             "</td></tr>" +
             "</table>"
         );
       },
-      style: myStyle,
+      style: style,
     }).addTo(map);
   })
   .catch((error) => {
